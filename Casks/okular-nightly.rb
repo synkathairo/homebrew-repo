1c213d4ffc3f3e6ec40b13cf048f86d75c13b01f
synkathairo <20174932+synkathairo@users.noreply.github.com>
2026-04-25 19:11:37 -0400

    okular-nightly
    add okular nightly cask (see https://okular.kde.org/download/)

---
+++ Casks/okular-nightly.rb
@@ -0,0 +1,32 @@
+cask "okular-nightly" do
+  arch arm: "arm64", intel: "x86_64"
+
+  version "7316"
+  sha256 :no_check
+
+  url "https://cdn.kde.org/ci-builds/graphics/okular/master/macos-#{arch}/okular-master-#{version}-macos-clang-#{arch}.dmg"
+  name "Okular Nightly"
+  desc "KDE;s Universal Document Viewer"
+  homepage "https://okular.kde.org/"
+
+  livecheck do
+    url "https://cdn.kde.org/ci-builds/graphics/okular/master/macos-arm64/"
+    regex(/okular-master-(\d+)-macos-clang-arm64\.dmg/i)
+  end
+
+  app "okular.app"
+
+  zap trash: [
+    "~/Library/Application Support/okular",
+    "~/Library/Preferences/okular.plist",
+    "~/Library/Preferences/okularpartrc",
+    "~/Library/Preferences/okularrc",
+    "~/Library/Preferences/org.kde.okular.plist",
+  ]
+
+  caveats <<~EOS
+    This installs an unsigned nightly build from KDE CI.
+    You may need to manually allow it by running
+    `xattr -rd com.apple.quarantine /Applications/okular.app`
+  EOS
+end
---
+++ README.md
@@ -0,0 +1,9 @@
+# Homebrew repository
+
+some useful software for macOS
+
+## Notes
+
+Some software may be unsigned, and requires running, for example 
+
+`xattr -rd com.apple.quarantine /Applications/okular.app`
