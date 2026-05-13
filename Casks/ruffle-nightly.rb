c262591bc5875ae9d567ce67f66ac268b6f2dce0
synkathairo <20174932+synkathairo@users.noreply.github.com>
2026-05-13 16:36:10 -0400

    ruffle-nightly 2026_05_13

---
+++ Casks/ruffle-nightly.rb
@@ -0,0 +1,28 @@
+cask "ruffle-nightly" do
+  version "2026_05_13"
+  sha256 "45f09531046469cf53b35c0039ab636aae2942c9170c84a72e23bda1155a9a53"
+
+  url "https://github.com/ruffle-rs/ruffle/releases/download/nightly-#{version.tr("_", "-")}/ruffle-nightly-#{version}-macos-universal.tar.gz",
+      verified: "github.com/ruffle-rs/ruffle/"
+  name "ruffle-nightly"
+  desc "Open source Flash Player emulator"
+  homepage "https://ruffle.rs/"
+
+  livecheck do
+    url "https://github.com/ruffle-rs/ruffle.git"
+    regex(/^nightly-(\d{4}-\d{2}-\d{2})$/i)
+    strategy :git do |tags, regex|
+      tags.filter_map { |tag| tag[regex, 1]&.tr("-", "_") }
+          .max
+    end
+  end
+
+  depends_on macos: :big_sur
+
+  app "Ruffle.app"
+
+  zap trash: [
+    "~/Library/Application Support/ruffle",
+    "~/Library/Caches/ruffle",
+  ]
+end
