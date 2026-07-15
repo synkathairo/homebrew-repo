6caf242aaa5b2ab918b5e74199c59222793d377f
synkathairo <20174932+synkathairo@users.noreply.github.com>
2026-07-14 22:09:32 -0400

    container 1.1.0
    force support for macos sequoia

---
+++ Formula/container.rb
@@ -0,0 +1,92 @@
+class Container < Formula
+  desc "Create and run Linux containers using lightweight virtual machines"
+  homepage "https://apple.github.io/container/documentation/"
+  url "https://github.com/apple/container/archive/refs/tags/1.1.0.tar.gz"
+  sha256 "beb7f2536d714de6863a056b677a2b51aa498bb7caec6011cfb01c0c95bcbe8c"
+  license "Apache-2.0"
+  head "https://github.com/apple/container.git", branch: "main"
+
+  depends_on xcode: ["26.0", :build]
+  depends_on arch: :arm64
+  depends_on macos: :sequoia
+
+  def install
+    if build.head?
+      ENV["GIT_COMMIT"] = Utils.git_head
+    else
+      ENV["RELEASE_VERSION"] = version
+    end
+
+    system "swift", "build", "--disable-sandbox", "--configuration", "release"
+
+    release_dir = buildpath/".build/release"
+
+    bin.install release_dir/"container"
+    bin.install release_dir/"container-apiserver"
+    libexec.install "scripts/ensure-container-stopped.sh"
+
+    # Container requires binaries and plugins to be signed with specific entitlements
+    codesign "--identifier=com.apple.container.cli", bin/"container"
+    codesign "--identifier=com.apple.container.apiserver", bin/"container-apiserver"
+
+    plugins = {
+      "container-core-images"   => { source: "CoreImages",       entitlements: false },
+      "container-network-vmnet" => { source: "NetworkVMNet",     entitlements: true  },
+      "container-runtime-linux" => { source: "RuntimeLinux",     entitlements: true  },
+      "machine-apiserver"       => { source: "MachineAPIServer", entitlements: false,
+                                     resources: ["init", "create-user.sh"] },
+    }
+    plugins.each do |bin_name, opts|
+      plugin_dir = libexec/"container-plugins/#{bin_name}"
+      (plugin_dir/"bin").install release_dir/bin_name
+      plugin_dir.install "Sources/Plugins/#{opts[:source]}/config.toml"
+      opts[:resources]&.each do |resource|
+        (plugin_dir/"resources").install "Sources/Plugins/#{opts[:source]}/Resources/#{resource}"
+      end
+
+      entitlement_args = []
+      entitlement_args << "--entitlements=signing/#{bin_name}.entitlements" if opts[:entitlements]
+
+      codesign "--prefix=com.apple.container.", *entitlement_args,
+               plugin_dir/"bin/#{bin_name}"
+    end
+
+    generate_completions_from_executable bin/"container", "--generate-completion-script"
+
+    # Relocate the binaries under libexec and replace them in bin with shim
+    # wrappers. The CLI resolves its install root as the lexical grandparent
+    # of the running executable (see Sources/ContainerPlugin/InstallRoot.swift),
+    # so the binaries must live one directory below the keg root for the
+    # bundled plugins under libexec/container-plugins/ to be discovered.
+    # CONTAINER_INSTALL_ROOT is also exported by the wrappers for the code
+    # paths that honour the environment variable.
+    bin.env_script_all_files libexec, CONTAINER_INSTALL_ROOT: opt_prefix
+  end
+
+  def codesign(*args)
+    system "/usr/bin/codesign", "-f", "-s", "-", *args
+  end
+
+  # container APIs aren't guaranteed to be backward compatible,
+  # so we stop the system service to ensure no components are out of sync.
+  # Ref: https://github.com/apple/container/issues/551#issuecomment-3246928923
+  def post_install
+    system libexec/"ensure-container-stopped.sh", "-a"
+  end
+
+  service do
+    run [opt_bin/"container", "system", "start"]
+    keep_alive true
+    working_dir var
+    log_path var/"log/container.log"
+    error_log_path var/"log/container.log"
+  end
+
+  test do
+    # Cannot fully test, as it needs to write outside testpath
+    assert_match version.to_s, shell_output("#{bin}/container --version")
+
+    assert_match(/Error: (?:interrupted: ")?internalError: "failed to list containers"/,
+                 shell_output("#{bin}/container list 2>&1", 1))
+  end
+end
