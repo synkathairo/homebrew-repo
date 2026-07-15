19575cbd17f9f0b2de57bbf4c51a4732ec8278cd
synkathairo <20174932+synkathairo@users.noreply.github.com>
2026-07-14 22:15:36 -0400

    treesheets 3255
    remove the deprecation / disable block from upstream (note: unsigned)

---
+++ Casks/treesheets.rb
@@ -0,0 +1,39 @@
+cask "treesheets" do
+  version "3255"
+  sha256 "a0afc8414e78fc222ab22a14d7fe6cd7d46540273c9758dd4c2e0c21780b1a59"
+
+  url "https://github.com/aardappel/treesheets/releases/download/#{version.csv.second || version.csv.first}/TreeSheets-#{version.csv.first}-Darwin.dmg",
+      verified: "github.com/aardappel/treesheets/"
+  name "TreeSheets"
+  desc "Hierarchical spreadsheet and outline application"
+  homepage "https://strlen.com/treesheets/"
+
+  livecheck do
+    url :url
+    regex(%r{/v?(\d+(?:\.\d+)*)/TreeSheets[._-]v?(\d+(?:\.\d+)*)(?:[._-]Darwin)?\.dmg$}i)
+    strategy :github_latest do |json, regex|
+      json["assets"]&.map do |asset|
+        match = asset["browser_download_url"]&.match(regex)
+        next if match.blank?
+
+        (match[2] == match[1]) ? match[1] : "#{match[2]},#{match[1]}"
+      end
+    end
+  end
+
+  depends_on :macos
+
+  app "TreeSheets.app"
+
+  uninstall quit: "dot3labs.TreeSheets"
+
+  zap trash: [
+    "~/Library/Preferences/dot3labs.TreeSheets.plist",
+    "~/Library/Preferences/TreeSheets Preferences",
+    "~/Library/Saved Application State/dot3labs.TreeSheets.savedState",
+  ]
+
+  caveats do
+    unsigned_accessibility
+  end
+end
