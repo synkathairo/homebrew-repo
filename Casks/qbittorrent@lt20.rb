4575464da6f27cdc38d113f111ffb48015c0e54a
synkathairo <20174932+synkathairo@users.noreply.github.com>
2026-09-10 21:16:43 -0400

    fixes and new

--- Casks/qbittorrent@lt20.rb
+++ Casks/qbittorrent@lt20.rb
@@ -1,11 +1,11 @@
-cask "qbittorrent" do
+cask "qbittorrent@lt20" do
   arch intel: "x86_64"
   os macos: "mac", linux: "appimage"
   url_end = on_system_conditional macos: ".dmg", linux: "_#{arch}.AppImage"
 
   on_monterey :or_older do
     version "5.0.5"
-    sha256 "7285e98b5ef4b9a227247dcd933f1a556bfc1ff10901fc7aedb0c4b7ad082814"
+    sha256 "f0180356f89590d2de639ba700813fb9ba1d90dde1af5353fc7621a048cccbb2"
 
     livecheck do
       skip "Legacy version"
@@ -13,11 +13,11 @@ cask "qbittorrent" do
   end
   on_ventura :or_newer do
     version "5.2.3"
-    sha256 "9e37f6c7ff848c7bdd3c10167614c0cb78c00e2ddcc323f1ad3ac6c008a0481f"
+    sha256 "4dde4f357cf8d7271f7e25140d2986d076126566edf2d2ad441cdd382b2a333d"
 
     livecheck do
       url "https://sourceforge.net/projects/qbittorrent/rss?path=/qbittorrent-#{os}"
-      regex(%r{url=.*?/qbittorrent[._-]v?(\d+(?:\.\d+)+)#{url_end}}i)
+      regex(%r{url=.*?/qbittorrent[._-]v?(\d+(?:\.\d+)+)[._-]lt20#{url_end}}i)
     end
   end
   on_macos do
@@ -35,25 +35,25 @@ cask "qbittorrent" do
   end
   on_linux do
     version "5.2.3"
-    sha256 "c1467a713929323aaf253e021449992ac299a6c830a933643b023007d8641ed0"
+    sha256 "71b3a861753674d941e517feff72ed47d1a0e5c01a0a39e9c3d7f7ccc3f80c63"
 
     livecheck do
       url "https://sourceforge.net/projects/qbittorrent/rss?path=/qbittorrent-#{os}"
-      regex(%r{url=.*?/qbittorrent[._-]v?(\d+(?:\.\d+)+)#{url_end}}i)
+      regex(%r{url=.*?/qbittorrent[._-]v?(\d+(?:\.\d+)+)[._-]lt20#{url_end}}i)
     end
 
     depends_on arch: :x86_64
 
-    app_image "qbittorrent-#{version}_#{arch}.AppImage", target: "qBittorrent.AppImage"
+    app_image "qbittorrent-#{version}_lt20_#{arch}.AppImage", target: "qBittorrent.AppImage"
   end
 
-  url "https://downloads.sourceforge.net/qbittorrent/qbittorrent-#{os}/qbittorrent-#{version}/qbittorrent-#{version}#{url_end}"
+  url "https://downloads.sourceforge.net/qbittorrent/qbittorrent-#{os}/qbittorrent-#{version}/qbittorrent-#{version}_lt20#{url_end}"
   name "qBittorrent"
-  desc "Peer to peer Bitorrent client"
+  desc "Edition of qBitorrent based on libtorrent-rasterbar 2.0.x"
   homepage "https://www.qbittorrent.org/"
 
   conflicts_with cask: [
     "c0re100-qbittorrent",
-    "qbittorrent@lt20",
+    "qbittorrent",
   ]
 end
