a05e149d57a85c0f45fa844ef1c203f4ece7310b
synkathairo <20174932+synkathairo@users.noreply.github.com>
2026-09-10 21:18:12 -0400

    Update ruffle-nightly.rb

--- Casks/ruffle-nightly.rb
+++ Casks/ruffle-nightly.rb
@@ -2,7 +2,7 @@ cask "ruffle-nightly" do
   version "2026_08_30"
   sha256 "ee68a9b7b62321db1107569b9a4b084c0a08c75703d12ecb831b0804064edc54"
 
-  url "https://github.com/ruffle-rs/ruffle/releases/download/nightly-#{version.tr("_", "-")}/ruffle-nightly-#{version}-macos-universal.tar.gz",
+  url "https://github.com/ruffle-rs/ruffle/releases/download/nightly-#{version.tr("_", "-")}/ruffle-nightly-#{version}-macos-universal.tar.gz"
   name "ruffle-nightly"
   desc "Open source Flash Player emulator"
   homepage "https://ruffle.rs/"
